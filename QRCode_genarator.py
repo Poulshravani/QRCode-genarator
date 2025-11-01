1d8c055fa95366c26322b7e3d326453a27715e06
Poulshravani <poulshravani2@gmail.com>
2025-11-01 16:03:57 +0530

    Add project files.

---
+++ QRCode_genarator.py
@@ -0,0 +1,31 @@
+import qrcode
+
+#Taking UPI ID as a input
+upi_id = input("Enter Your UPI ID = ")
+
+#upi://pay?pa=UPI_ID&apn=NAME&am=AMOUNT&cu=CURRENCY&tn=MESSAGE
+
+#Define the payment URL based on the UPI ID And the payment app
+#You can modify these URLs based on the payment apps you want to support
+
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+#Create QR Code for each payment app
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+#Save the QR Code to image file(optional)
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+#Display the QR Codes (you may need to install PIL?Pillow Library)
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
+
+
+
